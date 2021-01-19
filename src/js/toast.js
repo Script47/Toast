b4fed78a190435d4d7f62d66ed92a34fad648189
SoyaNyan <nyan@soya.moe>
2021-01-19 20:46:08 +0900

    플러그인 초기화 메서드 toastInit 추가

--- src/js/toast.js
+++ src/js/toast.js
@@ -8,20 +8,20 @@
     const TOAST_CONTAINER_HTML = `<div id="toast-container" class="toast-container" aria-live="polite" aria-atomic="true"></div>`;
 
     $.toastDefaults = {
-        position: 'top-right',
+        position: "top-right",
         dismissible: true,
-        stackable: true,
+        stackable: true, // stackable & pauseDelayOnHover options are incompatible
         pauseDelayOnHover: true,
         style: {
-            toast: '',
-            info: '',
-            success: '',
-            warning: '',
-            error: '',
-        }
+            toast: "",
+            info: "",
+            success: "",
+            warning: "",
+            error: "",
+        },
     };
 
-    $('body').on('hidden.bs.toast', '.toast', () => {
+    $("body").on("hidden.bs.toast", ".toast", () => {
         $(this).remove();
     });
 
@@ -29,22 +29,31 @@
 
     function render(opts) {
         /** No container, create our own **/
-        if (!$('#toast-container').length) {
-            const position = ['top-right', 'top-left', 'top-center', 'bottom-right', 'bottom-left', 'bottom-center'].includes($.toastDefaults.position) ? $.toastDefaults.position : 'top-right';
+        if (!$("#toast-container").length) {
+            const position = [
+                "top-right",
+                "top-left",
+                "top-center",
+                "bottom-right",
+                "bottom-left",
+                "bottom-center",
+            ].includes($.toastDefaults.position)
+                ? $.toastDefaults.position
+                : "top-right";
 
-            $('body').prepend(TOAST_CONTAINER_HTML);
-            $('#toast-container').addClass(position);
+            $("body").prepend(TOAST_CONTAINER_HTML);
+            $("#toast-container").addClass(position);
         }
 
-        let toastContainer = $('#toast-container');
-        let html = '';
+        let toastContainer = $("#toast-container");
+        let html = "";
         let classes = {
             header: {
-                fg: '',
-                bg: ''
+                fg: "",
+                bg: "",
             },
-            subtitle: 'text-white',
-            dismiss: 'text-white'
+            subtitle: "text-white",
+            dismiss: "text-white",
         };
         let id = opts.id || `toast-${toastRunningCount}`;
         let type = opts.type;
@@ -52,48 +61,57 @@
         let subtitle = opts.subtitle;
         let content = opts.content;
         let img = opts.img;
-        let delayOrAutohide = opts.delay ? `data-delay="${opts.delay}"` : `data-autohide="false"`;
+        let delayOrAutohide = opts.delay
+            ? `data-delay="${opts.delay}"`
+            : `data-autohide="false"`;
         let hideAfter = ``;
         let dismissible = $.toastDefaults.dismissible;
         let globalToastStyles = $.toastDefaults.style.toast;
         let paused = false;
 
-        if (typeof opts.dismissible !== 'undefined') {
+        if (typeof opts.dismissible !== "undefined") {
             dismissible = opts.dismissible;
         }
 
         switch (type) {
-            case 'info':
-                classes.header.bg = $.toastDefaults.style.info || 'bg-info';
-                classes.header.fg = $.toastDefaults.style.info || 'text-white';
+            case "info":
+                classes.header.bg = $.toastDefaults.style.info || "bg-info";
+                classes.header.fg = $.toastDefaults.style.info || "text-white";
                 break;
 
-            case 'success':
-                classes.header.bg = $.toastDefaults.style.success || 'bg-success';
-                classes.header.fg = $.toastDefaults.style.info || 'text-white';
+            case "success":
+                classes.header.bg =
+                    $.toastDefaults.style.success || "bg-success";
+                classes.header.fg = $.toastDefaults.style.info || "text-white";
                 break;
 
-            case 'warning':
-                classes.header.bg = $.toastDefaults.style.warning || 'bg-warning';
-                classes.header.fg = $.toastDefaults.style.warning || 'text-white';
+            case "warning":
+                classes.header.bg =
+                    $.toastDefaults.style.warning || "bg-warning";
+                classes.header.fg =
+                    $.toastDefaults.style.warning || "text-white";
                 break;
 
-            case 'error':
-                classes.header.bg = $.toastDefaults.style.error || 'bg-danger';
-                classes.header.fg = $.toastDefaults.style.error || 'text-white';
+            case "error":
+                classes.header.bg = $.toastDefaults.style.error || "bg-danger";
+                classes.header.fg = $.toastDefaults.style.error || "text-white";
                 break;
         }
 
         if ($.toastDefaults.pauseDelayOnHover && opts.delay) {
             delayOrAutohide = `data-autohide="false"`;
-            hideAfter = `data-hide-after="${Math.floor(Date.now() / 1000) + (opts.delay / 1000)}"`;
+            hideAfter = `data-hide-after="${
+                Math.floor(Date.now() / 1000) + opts.delay / 1000
+            }"`;
         }
 
         html = `<div id="${id}" class="toast ${globalToastStyles}" role="alert" aria-live="assertive" aria-atomic="true" ${delayOrAutohide} ${hideAfter}>`;
         html += `<div class="toast-header ${classes.header.bg} ${classes.header.fg}">`;
 
         if (img) {
-            html += `<img src="${img.src}" class="mr-2 ${img.class || ''}" alt="${img.alt || 'Image'}">`;
+            html += `<img src="${img.src}" class="mr-2 ${
+                img.class || ""
+            }" alt="${img.alt || "Image"}">`;
         }
 
         html += `<strong class="mr-auto">${title}</strong>`;
@@ -119,42 +137,58 @@
         html += `</div>`;
 
         if (!$.toastDefaults.stackable) {
-            toastContainer.find('.toast').each(() => {
+            toastContainer.find(".toast").each(() => {
                 $(this).remove();
             });
 
             toastContainer.append(html);
-            toastContainer.find('.toast:last').toast('show');
+            toastContainer.find(".toast:last").toast("show");
         } else {
             toastContainer.append(html);
-            toastContainer.find('.toast:last').toast('show');
+            toastContainer.find(".toast:last").toast("show");
         }
 
         if ($.toastDefaults.pauseDelayOnHover) {
             setTimeout(() => {
                 if (!paused) {
-                    $(`#${id}`).toast('hide');
+                    $(`#${id}`).toast("hide");
                 }
             }, opts.delay);
 
-            $('body').on('mouseover', `#${id}`, () => {
+            $("body").on("mouseover", `#${id}`, () => {
                 paused = true;
             });
 
-            $(document).on('mouseleave', '#' + id, () => {
+            $(document).on("mouseleave", "#" + id, () => {
                 const current = Math.floor(Date.now() / 1000),
-                    future = parseInt($(this).data('hideAfter'));
+                    future = parseInt($(this).data("hideAfter"));
 
                 paused = false;
 
                 if (current >= future) {
-                    $(this).toast('hide');
+                    $(this).toast("hide");
                 }
             });
         }
 
         toastRunningCount++;
     }
+
+    /**
+     * Set default option values
+     * @param opts
+     */
+    $.toastInit = function (opts) {
+        const { position, dismissible, stackable, pauseDelayOnHover } = opts;
+
+        // set values
+        $.toastDefaults.position = position ?? $.toastDefaults.position;
+        $.toastDefaults.dismissible =
+            dismissible ?? $.toastDefaults.dismissible;
+        $.toastDefaults.stackable = stackable ?? $.toastDefaults.stackable;
+        $.toastDefaults.pauseDelayOnHover =
+            pauseDelayOnHover ?? $.toastDefaults.pauseDelayOnHover;
+    };
 
     /**
      * Show a snack
@@ -166,9 +200,9 @@
         return render({
             type,
             title,
-            delay
+            delay,
         });
-    }
+    };
 
     /**
      * Show a toast
@@ -176,5 +210,5 @@
      */
     $.toast = function (opts) {
         return render(opts);
-    }
-}(jQuery));
+    };
+})(jQuery);
